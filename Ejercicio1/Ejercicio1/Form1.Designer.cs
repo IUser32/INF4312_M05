eba7a1b9da1989f9d4755e4c02473c0426dd6833
Luis Fortuna <javier-uribe@hotmail.com>
2025-09-30 17:59:41 -0400

    agregando 2 checkboxs.

--- Ejercicio1/Ejercicio1/Form1.Designer.cs
+++ Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             button1 = new Button();
             label1 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
             SuspendLayout();
             // 
             // button1
@@ -50,11 +52,33 @@
             label1.TabIndex = 1;
             label1.Text = "label1";
             // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(29, 45);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(197, 45);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(309, 46);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(197, 45);
+            checkBox2.TabIndex = 3;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form1";
@@ -67,5 +91,7 @@
 
         private Button button1;
         private Label label1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
     }
 }
