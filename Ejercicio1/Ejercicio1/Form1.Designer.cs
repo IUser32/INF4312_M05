1741c04ee95038a42ad5567be5ad3f9f65fb2884
Luis Fortuna <javier-uribe@hotmail.com>
2025-10-01 05:40:59 -0400

    test

--- Ejercicio1/Ejercicio1/Form1.Designer.cs
+++ Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -28,70 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            label1 = new Label();
-            checkBox1 = new CheckBox();
-            checkBox2 = new CheckBox();
+            dgvEstudiantes = new DataGridView();
+            Matricula = new DataGridViewTextBoxColumn();
+            Nombres = new DataGridViewTextBoxColumn();
+            Apellidos = new DataGridViewTextBoxColumn();
+            Sexo = new DataGridViewTextBoxColumn();
+            Materia = new DataGridViewTextBoxColumn();
+            lblName = new Label();
+            lblNombres = new Label();
+            txtNombres = new TextBox();
+            lblApellidos = new Label();
+            txtApellidos = new TextBox();
+            lblMateria = new Label();
+            lblSexo = new Label();
+            cmbSexo = new ComboBox();
+            cmbMateria = new ComboBox();
+            btnGuardar = new Button();
+            btnLimpiar = new Button();
+            mtbtMatricula = new MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvEstudiantes).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // dgvEstudiantes
             // 
-            button1.Location = new Point(182, 132);
-            button1.Name = "button1";
-            button1.Size = new Size(367, 208);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            dgvEstudiantes.AllowUserToAddRows = false;
+            dgvEstudiantes.AllowUserToDeleteRows = false;
+            dgvEstudiantes.AllowUserToOrderColumns = true;
+            dgvEstudiantes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEstudiantes.Columns.AddRange(new DataGridViewColumn[] { Matricula, Nombres, Apellidos, Sexo, Materia });
+            dgvEstudiantes.Location = new Point(40, 308);
+            dgvEstudiantes.Name = "dgvEstudiantes";
+            dgvEstudiantes.ReadOnly = true;
+            dgvEstudiantes.RowHeadersWidth = 102;
+            dgvEstudiantes.Size = new Size(1360, 375);
+            dgvEstudiantes.TabIndex = 0;
             // 
-            // label1
+            // Matricula
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(645, 279);
-            label1.Name = "label1";
-            label1.Size = new Size(97, 41);
-            label1.TabIndex = 1;
-            label1.Text = "label1";
+            Matricula.HeaderText = "Matricula";
+            Matricula.MinimumWidth = 12;
+            Matricula.Name = "Matricula";
+            Matricula.ReadOnly = true;
+            Matricula.Width = 250;
             // 
-            // checkBox1
+            // Nombres
             // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(29, 45);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(197, 45);
-            checkBox1.TabIndex = 2;
-            checkBox1.Text = "checkBox1";
-            checkBox1.UseVisualStyleBackColor = true;
+            Nombres.HeaderText = "Nombres";
+            Nombres.MinimumWidth = 12;
+            Nombres.Name = "Nombres";
+            Nombres.ReadOnly = true;
+            Nombres.Width = 250;
             // 
-            // checkBox2
+            // Apellidos
             // 
-            checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(309, 46);
-            checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(197, 45);
-            checkBox2.TabIndex = 3;
-            checkBox2.Text = "checkBox2";
-            checkBox2.UseVisualStyleBackColor = true;
+            Apellidos.HeaderText = "Apellidos";
+            Apellidos.MinimumWidth = 12;
+            Apellidos.Name = "Apellidos";
+            Apellidos.ReadOnly = true;
+            Apellidos.Width = 250;
+            // 
+            // Sexo
+            // 
+            Sexo.HeaderText = "Sexo";
+            Sexo.MinimumWidth = 12;
+            Sexo.Name = "Sexo";
+            Sexo.ReadOnly = true;
+            Sexo.Width = 250;
+            // 
+            // Materia
+            // 
+            Materia.HeaderText = "Materia";
+            Materia.MinimumWidth = 12;
+            Materia.Name = "Materia";
+            Materia.ReadOnly = true;
+            Materia.Width = 250;
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(38, 25);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(140, 41);
+            lblName.TabIndex = 2;
+            lblName.Text = "Matricula";
+            // 
+            // lblNombres
+            // 
+            lblNombres.AutoSize = true;
+            lblNombres.Location = new Point(433, 25);
+            lblNombres.Name = "lblNombres";
+            lblNombres.Size = new Size(141, 41);
+            lblNombres.TabIndex = 4;
+            lblNombres.Text = "Nombres";
+            // 
+            // txtNombres
+            // 
+            txtNombres.Location = new Point(433, 69);
+            txtNombres.Name = "txtNombres";
+            txtNombres.Size = new Size(378, 47);
+            txtNombres.TabIndex = 3;
+            // 
+            // lblApellidos
+            // 
+            lblApellidos.AutoSize = true;
+            lblApellidos.Location = new Point(826, 25);
+            lblApellidos.Name = "lblApellidos";
+            lblApellidos.Size = new Size(141, 41);
+            lblApellidos.TabIndex = 6;
+            lblApellidos.Text = "Apellidos";
+            // 
+            // txtApellidos
+            // 
+            txtApellidos.Location = new Point(826, 69);
+            txtApellidos.Name = "txtApellidos";
+            txtApellidos.Size = new Size(420, 47);
+            txtApellidos.TabIndex = 5;
+            // 
+            // lblMateria
+            // 
+            lblMateria.AutoSize = true;
+            lblMateria.Location = new Point(433, 133);
+            lblMateria.Name = "lblMateria";
+            lblMateria.Size = new Size(118, 41);
+            lblMateria.TabIndex = 8;
+            lblMateria.Text = "Materia";
+            // 
+            // lblSexo
+            // 
+            lblSexo.AutoSize = true;
+            lblSexo.Location = new Point(38, 133);
+            lblSexo.Name = "lblSexo";
+            lblSexo.Size = new Size(82, 41);
+            lblSexo.TabIndex = 7;
+            lblSexo.Text = "Sexo";
+            // 
+            // cmbSexo
+            // 
+            cmbSexo.FormattingEnabled = true;
+            cmbSexo.Items.AddRange(new object[] { "", "Masculino", "Femenina" });
+            cmbSexo.Location = new Point(38, 177);
+            cmbSexo.Name = "cmbSexo";
+            cmbSexo.Size = new Size(378, 49);
+            cmbSexo.TabIndex = 9;
+            // 
+            // cmbMateria
+            // 
+            cmbMateria.FormattingEnabled = true;
+            cmbMateria.Items.AddRange(new object[] { "", "INF 4312", "INF 4311", "INF 4313", "INF 4319" });
+            cmbMateria.Location = new Point(433, 177);
+            cmbMateria.Name = "cmbMateria";
+            cmbMateria.Size = new Size(378, 49);
+            cmbMateria.TabIndex = 10;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(1047, 163);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(199, 75);
+            btnGuardar.TabIndex = 11;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(826, 163);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(204, 75);
+            btnLimpiar.TabIndex = 12;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // mtbtMatricula
+            // 
+            mtbtMatricula.Location = new Point(38, 69);
+            mtbtMatricula.Mask = "SO-0000-0000";
+            mtbtMatricula.Name = "mtbtMatricula";
+            mtbtMatricula.Size = new Size(378, 47);
+            mtbtMatricula.TabIndex = 13;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(checkBox2);
-            Controls.Add(checkBox1);
-            Controls.Add(label1);
-            Controls.Add(button1);
+            ClientSize = new Size(1412, 704);
+            Controls.Add(mtbtMatricula);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnGuardar);
+            Controls.Add(cmbMateria);
+            Controls.Add(cmbSexo);
+            Controls.Add(lblMateria);
+            Controls.Add(lblSexo);
+            Controls.Add(lblApellidos);
+            Controls.Add(txtApellidos);
+            Controls.Add(lblNombres);
+            Controls.Add(txtNombres);
+            Controls.Add(lblName);
+            Controls.Add(dgvEstudiantes);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Formulario 1";
+            ((System.ComponentModel.ISupportInitialize)dgvEstudiantes).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private Label label1;
-        private CheckBox checkBox1;
-        private CheckBox checkBox2;
+        private DataGridView dgvEstudiantes;
+        private DataGridViewTextBoxColumn Matricula;
+        private DataGridViewTextBoxColumn Nombres;
+        private DataGridViewTextBoxColumn Apellidos;
+        private DataGridViewTextBoxColumn Sexo;
+        private DataGridViewTextBoxColumn Materia;
+        private Label lblName;
+        private Label lblNombres;
+        private TextBox txtNombres;
+        private Label lblApellidos;
+        private TextBox txtApellidos;
+        private Label lblMateria;
+        private Label lblSexo;
+        private ComboBox cmbSexo;
+        private ComboBox cmbMateria;
+        private Button btnGuardar;
+        private Button btnLimpiar;
+        private MaskedTextBox mtbtMatricula;
     }
 }
--- Ejercicio1/Ejercicio1/Form1.cs
+++ Ejercicio1/Ejercicio1/Form1.cs
@@ -6,5 +6,48 @@ namespace Ejercicio1
         {
             InitializeComponent();
         }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNombres.Clear();
+            mtbtMatricula.Clear();
+            txtApellidos.Clear();
+
+            cmbSexo.SelectedIndex = 0;
+            cmbMateria.SelectedIndex = 0;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNombres.Text))
+            {
+                MessageBox.Show("El nombre es requerido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(txtApellidos.Text))
+            {
+                MessageBox.Show("El apellido es requerido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(mtbtMatricula.Text))
+            {
+                MessageBox.Show("La matricula es requerida.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (cmbSexo.SelectedIndex < 0)
+            {
+                MessageBox.Show("El sexo es requerido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (cmbMateria.SelectedIndex < 0)
+            {
+                MessageBox.Show("La materia es requerida.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            dgvEstudiantes.Rows.Add(mtbtMatricula.Text, txtNombres.Text, txtApellidos.Text, cmbSexo.SelectedItem.ToString(), cmbMateria.SelectedItem.ToString());
+
+            this.btnLimpiar_Click(sender, e);
+        }
     }
 }
